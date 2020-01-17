4eb812fb99b2e19942dc68b8e11c15664d4cee93
Eric Scott <me@ericscott.co.uk>
2020-01-17 17:09:45 +0000

    Added location pairs

--- src/main/java/uk/co/ericscott/ultralibs/location/LocationPair.java
+++ src/main/java/uk/co/ericscott/ultralibs/location/LocationPair.java
@@ -1,9 +1,10 @@
 package uk.co.ericscott.ultralibs.location;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter @Setter @AllArgsConstructor
 public class LocationPair
 {
     private LazyLocation loc1, loc2;
