6c4c77d122ec1aba711ca52494fcd2c3002ebd76
Eric <me@ericscott.co.uk>
2020-05-15 19:11:44 +0100

    Update RedisMessenger.java

--- src/main/java/uk/co/ericscott/ultralibs/redis/RedisMessenger.java
+++ src/main/java/uk/co/ericscott/ultralibs/redis/RedisMessenger.java
@@ -22,6 +22,10 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+* Credit to Ptech for this cool redis messenger
+*/
+
 @Getter
 public class RedisMessenger {
 
